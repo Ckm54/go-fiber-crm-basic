85cbd910fa3acaa82be7057002347d5a0f897a60
collo <mutaic84@gmail.com>
2022-11-04 18:45:05 +0300

    get a lead by id

--- lead/lead.go
+++ lead/lead.go
@@ -1,6 +1,11 @@
 package lead
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/ckm54/go-fiber-crm-basic/database"
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gofiber/fiber"
+)
 
 type Lead struct{
 	gorm.Model
@@ -8,4 +13,24 @@ type Lead struct{
 	Company 	string
 	Email 		string
 	Phone 		int
+}
+
+func GetLeads(c *fiber.Ctx){
+
+}
+
+func getLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx){
+
+}
+
+func DeleteLead(c *fiber.Ctx){
+
 }
