4486a6e45cb0fc5e9084539047cfd0b0126e5631
collo <mutaic84@gmail.com>
2022-11-04 18:49:06 +0300

    create a new lead in database

--- lead/lead.go
+++ lead/lead.go
@@ -31,7 +31,14 @@ func getLead(c *fiber.Ctx){
 }
 
 func NewLead(c *fiber.Ctx){
-
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
 }
 
 func DeleteLead(c *fiber.Ctx){
